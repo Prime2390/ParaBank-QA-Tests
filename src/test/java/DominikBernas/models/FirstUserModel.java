452bc3aff9c3923d14ed6d9cef507989bf9f1b9a
Dominik Bernaś <dominikbernas@gmail.com>
2025-02-14 20:02:39 +0100

    Make model

--- src/test/java/DominikBernas/models/FirstUserModel.java
+++ src/test/java/DominikBernas/models/FirstUserModel.java
@@ -1,54 +1,18 @@
 package DominikBernas.models;
 
+import lombok.Getter;
+
+@Getter
 public class FirstUserModel {
-    private String firstName = "Dominik";
-    private String lastName = "Bernas";
-    private String address = "Adama Mickiewicz 14/10";
-    private String city = "Warszawa";
-    private String state = "Wielkopolski";
-    private String zip = "42-400";
-    private String phone = "696596965";
-    private String ssn = "415262514536";
-    private String username ="Tester2390";
-    private String password ="Testerpass2390";
+    private final String firstName = "Dominik";
+    private final String lastName = "Bernas";
+    private final String address = "Adama Mickiewicz 14/10";
+    private final String city = "Warszawa";
+    private final String state = "Wielkopolski";
+    private final String zip = "42-400";
+    private final String phone = "696596965";
+    private final String ssn = "415262514536";
+    private final String username = "Tester2390";
+    private final String password = "Testerpass2390";
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getSsn() {
-        return ssn;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getZip() {
-        return zip;
-    }
-
-    public String getState() {
-        return state;
-    }
 }
--- src/test/java/DominikBernas/models/RegisterModelFaker.java
+++ src/test/java/DominikBernas/models/RegisterModelFaker.java
@@ -1,71 +1,22 @@
 package DominikBernas.models;
 
 import com.github.javafaker.Faker;
+import lombok.Getter;
 
+@Getter
 public class RegisterModelFaker {
 
-   private Faker faker = new Faker();
-   private String firstName = faker.name().firstName();
-    private String lastName = faker.name().lastName();
-    private String address = faker.address().streetAddress();
-    private String city = faker.address().city();
-    private String state = faker.address().state();
-    private String country = faker.address().country();
-    private String zipCode = faker.address().zipCode();
-    private String phoneNumber = faker.phoneNumber().phoneNumber();
-    private String pesel = faker.idNumber().valid();
-    private String login = faker.name().username();
-    private String password = faker.internet().password();
-    private String confirmPassword = faker.internet().password();
-
-    public Faker getFaker() {
-        return faker;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public String getPesel() {
-        return pesel;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
+ private final Faker faker = new Faker();
+ private final String firstName = faker.name().firstName();
+ private final String lastName = faker.name().lastName();
+ private final String address = faker.address().streetAddress();
+ private final String city = faker.address().city();
+ private final String state = faker.address().state();
+ private final String country = faker.address().country();
+ private final String zipCode = faker.address().zipCode();
+ private final String phoneNumber = faker.phoneNumber().phoneNumber();
+ private final String pesel = faker.idNumber().valid();
+ private final String login = faker.name().username();
+ private final String password = faker.internet().password();
+ private final String confirmPassword = faker.internet().password();
 }
